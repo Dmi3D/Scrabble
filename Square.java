e9193f7baf72945ab65a13fed9c5b45c0ead0b3f
Dmi3D <dmitriydranko@hotmail.com>
2020-02-18 20:29:01 +0000

    Changed array to now store array of Square objects

---
+++ Square.java
@@ -0,0 +1,59 @@
+public class Square
+{
+    private char tile;
+    private int weight;
+    private boolean isWordScore;
+
+    public Square()
+    {
+        this.weight = 0;
+    }
+
+    public Square(int value, boolean isWordScore)
+    {
+        this.weight = value;
+        this.isWordScore = isWordScore;
+    }
+
+    public int getWeight()
+    {
+        return weight;
+    }
+
+    public char getTile()
+    {
+        return tile;
+    }
+
+    public void setTile(char tile)
+    {
+        this.tile = tile;
+    }
+
+    public boolean isWordScore()
+    {
+        return isWordScore;
+    }
+
+    public String getType()
+    {
+        boolean isWord = isWordScore();
+
+        if (getWeight() == 0)
+            return "none";
+
+        else if(isWord)
+            return "word";
+
+        else
+            return "letter";
+    }
+
+    public boolean isEmpty()
+    {
+        if(weight == 0)
+            return true;
+
+        return false;
+    }
+}
