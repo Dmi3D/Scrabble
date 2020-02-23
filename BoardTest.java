cc953611cb998d4b9fad1459cec5feeefc8236ba
antalandra <andra.antalberbecaru@ucdconnect.ie>
2020-02-23 18:15:29 +0000

    BoardTest continued..

--- BoardTest.java
+++ BoardTest.java
@@ -30,7 +30,7 @@ public class BoardTest
         Board.displayBoard();
         System.out.println();
 
-        System.out.println("\n******************************************** ILLEGAL WORD PLACEMENT ON BOARD TEST ********************************************\n");
+        System.out.println("\n******************************************** ILLEGAL WORD PLACEMENT OF FIRST WORD ********************************************\n");
         System.out.println("Attempting to place first word in places that don't go through the centre square of the board. Centre is (8, H).");
         System.out.println("Word to place consists of tiles in " + PlayerOne.getName() + "'s frame from index 0 to 4.");
         System.out.println("Attempting to place the word down the board starting at position (2, H).");
@@ -54,6 +54,8 @@ public class BoardTest
         canPlace = Board.placeWord( word.toString(), 'A', 3, 'D', PlayerOne );
         System.out.println("Method call to placeWord() should return false. Actual: " + canPlace + "\n");
 
+
+        System.out.println("\n********************************************* LEGAL WORD PLACEMENT OF FIRST WORD *********************************************\n");
         System.out.println("Placing the first word across the board starting at position (8, F), which goes through centre");
         canPlace = Board.placeWord( word.toString(), 'A', 8, 'F', PlayerOne );
         System.out.println("Method call to placeWord() should return true. Actual: " + canPlace + "\n");
@@ -63,9 +65,10 @@ public class BoardTest
         PlayerOne.getPlayerFrame().fillFrame( Pool );
         System.out.println("\n");
 
-        System.out.println("Attempting to place second word in illegal place.");
+
+        System.out.println("\n*************************************** PLACEMENT OF SECOND WORD AWAY FROM TILES ON BOARD *************************************\n");
+        System.out.println("Attempting to place second word down the board starting at position (1, F).");
         System.out.println("Word to place consists of tiles in " + PlayerTwo.getName() + "'s frame from index 2 to 6.");
-        System.out.println("Attempting to place the word down the board starting at position (1, F).");
         System.out.println("This move is illegal because it doesn't overlap or touch any of the tiles of the first word on the board");
         word = new StringBuilder();
         for ( int i = 2; i < 7; i++ )
@@ -76,6 +79,9 @@ public class BoardTest
         System.out.println("Method call to placeWord() should return false. Actual: " + canPlace + "\n");
         System.out.println("Board will still only contain the first word placed:");
         Board.displayBoard();
+
+
+        System.out.println("\n********************************************** LEGAL WORD PLACEMENT OF SECOND WORD *******************************************\n");
         System.out.println("Placing the word down the board starting at position (3, G).");
         System.out.println("This move is legal because it touches one of the tiles of the first word found at position (8, G).");
         canPlace = Board.placeWord( word.toString(), 'D', 3, 'G', PlayerTwo );
@@ -87,6 +93,8 @@ public class BoardTest
         System.out.println("\n");
         PlayerTwo.getPlayerFrame().fillFrame( Pool );
 
+
+        System.out.println("\n**************************************** PLACEMENT OF THIRD WORD GOING OUT OF BOUNDS *****************************************\n");
         System.out.println("Placing the third word on the board in illegal place.");
         System.out.println("Word to place consists of tiles in " + PlayerOne.getName() + "'s frame from index 0 to 6.");
         System.out.println("Attempting to place the word across the board starting at position (9, J).");
@@ -102,6 +110,8 @@ public class BoardTest
         Board.displayBoard();
         System.out.println("\n");
 
+
+        System.out.println("\n********************************************** LEGAL WORD PLACEMENT OF THIRD WORD ********************************************\n");
         System.out.println("Placing the third word across on the board starting at position (9, I). This is a legal move.");
         System.out.println("The word touches two tiles at positions (8, I) and (8, J) and fills all positions to the end of the board.");
         canPlace = Board.placeWord( word.toString(), 'A', 9, 'I', PlayerOne );
@@ -115,6 +125,7 @@ public class BoardTest
         System.out.println("\n");
 
 
+        System.out.println("\n********************************* PLACEMENT OF FOURTH WORD STARTING AT POSITION OUT OF BOUNDS ********************************\n");
         System.out.println("Attempting to place the fourth word down the board starting at position (-1, F).");
         System.out.println("Word to place consists of tiles in " + PlayerTwo.getName() + "'s frame from index 3 to 7.");
         System.out.println("The word would touche a tile at position (3, G) but its starting position is out of bounds.");
@@ -125,18 +136,24 @@ public class BoardTest
         }
         canPlace = Board.placeWord( word.toString(), 'D', -1, 'F', PlayerTwo );
         System.out.println("Method call to placeWord() should return false. Actual: " + canPlace + "\n");
-        System.out.println("Board will still only contain only three words:");
+        System.out.println("Board still contains three words:");
         Board.displayBoard();
         System.out.println("\n");
 
 
-//        System.out.println("Attempting to place the word down the board starting at position out of bounds (-1, G)");
-//        canPlace = Board.placeWord( word.toString(), 'D', -1, 'G', PlayerOne );
-//        System.out.println("Method call to placeWord() should return false. Actual: " + canPlace + "\n");
-
-
-
-
+        System.out.println("\n*********************** WORD PLACEMENT ON BOARD WHEN PLAYER DOESN'T HAVE NECESSARY TILES FOR THE WORD *************************\n");
+        System.out.println("Attempting to place the fifth word down the board. This word has letters which cannot be found in the player's frame.");
+        System.out.println("Word to place would be " + PlayerOne.getName() + "'s and it would be placed down the board starting at position (9, H)");
+        word = new StringBuilder();
+        for ( int i = 0; i < 7; i++ )
+        {
+            word.append( PlayerTwo.getPlayerFrame().getTile( i ) + 1 );
+        }
+        canPlace = Board.placeWord( word.toString(), 'D', 9, 'H', PlayerTwo );
+        System.out.println("Method call to placeWord() should return false. Actual: " + canPlace + "\n");
+        System.out.println("Board still contains four words:");
+        Board.displayBoard();
+        System.out.println("\n");
 
 
 
