499401fb1fabf05e11bc4eb17593c34660caae85
antalandra <andra.antalberbecaru@ucdconnect.ie>
2020-03-15 15:39:36 +0000

    Removed ChoiceButtons fxml file and controller. Modified boardGraphic and placeWordContent.

--- src/sample/MainController.java
+++ src/sample/MainController.java
@@ -3,6 +3,7 @@ package sample;
 import javafx.fxml.FXML;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
@@ -14,15 +15,30 @@ public class MainController
 {
 
     @FXML
-    private BorderPane mainPane;
-    
-    public void loadChoiceButtonsContent( javafx.event.ActionEvent actionEvent ) throws IOException
+    public BorderPane mainPane;
+
+
+    /*public void loadChoiceButtonsContent( javafx.event.ActionEvent actionEvent ) throws IOException
     {
+
+        //mainPane.getRight().
+
         System.out.println("Loading choice buttons content"); // debug
         ChoiceButtonsContent content = new ChoiceButtonsContent();
         mainPane.setRight( content.getChoiceButtonsContent() );
         BorderPane.setMargin( mainPane.getCenter(), new Insets( 0, 0, 10, 0 ) );
         BorderPane.setMargin( mainPane.getRight(), new Insets( 0, 0, 0, 10 ) );
+    }*/
+
+    @FXML
+    public void handlePlaceWordButton( javafx.event.ActionEvent actionEvent ) throws IOException
+    {
+        System.out.println("Place Word Button Clicked"); // debug
+        PlaceWordContent content = new PlaceWordContent();
+        mainPane.setRight( content.getPlaceWordContent() );
+        BorderPane.setMargin( mainPane.getCenter(), new Insets( 0, 0, 10, 0 ) );
+        BorderPane.setMargin( mainPane.getRight(), new Insets( 0, 0, 0, 10 ) );
+
     }
 
 
