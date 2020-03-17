c044c9b1ecf68509448edea7055e8e0886d587a5
antalandra <andra.antalberbecaru@ucdconnect.ie>
2020-03-17 18:16:02 +0000

    Refactored class names from UI into Scrabble and from MainController to UI. Improved ActionEvent import in class 'UI'

--- src/sample/Scrabble.java
+++ src/sample/Scrabble.java
@@ -1,19 +1,24 @@
 package sample;
 
-public class Scrabble
-{
-    // should be class that allows players to play Scrabble
-    public static void main( String[] args )
-    {
-        OpeningWindowController openingWindow = new OpeningWindowController();
-        Pool GamePool = new Pool();
-        Frame FrameOne = new Frame(GamePool);
-        Frame FrameTwo = new Frame(GamePool);
-        Player PlayerOne = new Player( FrameOne );
-        Player PlayerTwo = new Player( FrameTwo );
-        PlayerOne.setName( openingWindow.getPlayerOneName() );
-        PlayerTwo.setName( openingWindow.getPlayerTwoName() );
-        System.out.println("Player One: " + PlayerOne.getName());
-        System.out.println("Player Two: " + PlayerTwo.getName());
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Scrabble extends Application {
+
+    @Override
+    public void start(Stage window) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource( "openingWindow.fxml" ));
+        window.setTitle("Scrabble");
+        window.setScene(new Scene(root, 1028, 500));
+        window.setResizable(false);
+        window.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
--- src/sample/UI.java
+++ src/sample/UI.java
@@ -1,24 +1,91 @@
 package sample;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.DialogPane;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.*;
+import javafx.event.ActionEvent;
+import java.io.IOException;
 
-public class UI extends Application {
+public class UI
+{
+    @FXML
+    private BorderPane rightPanel;
 
-    @Override
-    public void start(Stage window) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource( "openingWindow.fxml" ));
-        window.setTitle("Scrabble");
-        window.setScene(new Scene(root, 1028, 500));
-        window.setResizable(false);
-        window.show();
+    @FXML
+    private TextArea wordsPlacedDisplay;
+
+    @FXML
+    private DialogPane playerInformationDisplayOnTurn;
+
+    @FXML
+    private Label playerTurnDisplayLabel;
+
+    @FXML
+    private GridPane playerTilesDisplayOnTurn;
+
+    @FXML
+    public void handleChallengeButton( ActionEvent actionEvent ) throws IOException
+    {
+        System.out.println("Challenge Button Clicked"); // debug
+        FxmlLoader content = new FxmlLoader();
+        rightPanel.setBottom( content.getChallengeContent() );
+        BorderPane.setMargin( rightPanel.getBottom(), new Insets( 0, 10, 10, 10 )  );
+    }
+
+    @FXML
+    public void handleExchangeButton( ActionEvent actionEvent ) throws IOException
+    {
+        System.out.println("Exchange Button Clicked"); // debug
+        FxmlLoader content = new FxmlLoader();
+        rightPanel.setBottom( content.getExchangeContent() );
+        BorderPane.setMargin( rightPanel.getBottom(), new Insets( 0, 10, 10, 10 )  );
+
+    }
+
+    @FXML
+    public void handlePlaceWordButton( ActionEvent actionEvent ) throws IOException
+    {
+        System.out.println("Place Word Button Clicked"); // debug
+        FxmlLoader content = new FxmlLoader();
+        rightPanel.setBottom( content.getPlaceWordContent() );
+        BorderPane.setMargin( rightPanel.getBottom(), new Insets( 0, 10, 10, 10 )  );
+    }
+
+    @FXML
+    public void handleHelpButton( ActionEvent actionEvent ) throws IOException
+    {
+        System.out.println("Help Button Clicked"); // debug
+        FxmlLoader content = new FxmlLoader();
+        rightPanel.setBottom( content.getHelpContent() );
+        BorderPane.setMargin( rightPanel.getBottom(), new Insets( 0, 10, 10, 10 )  );
+    }
+
+    @FXML
+    public void handleQuitButton( ActionEvent actionEvent ) throws IOException
+    {
+        System.out.println("Quit Button Clicked"); // debug
+        FxmlLoader content = new FxmlLoader();
+        rightPanel.setBottom( content.getQuitContent() );
+        BorderPane.setMargin( rightPanel.getBottom(), new Insets( 0, 10, 10, 10 )  );
+    }
+
+    @FXML
+    public void handlePassButton( ActionEvent actionEvent ) throws IOException
+    {
+        System.out.println( "Pass Button Clicked" ); // debug
+        // keep a counter for number of times pass is selected
+        // if selected twice in succession, game is over
+        // also make this button trigger next turn
     }
 
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+
+
+
+
 }
